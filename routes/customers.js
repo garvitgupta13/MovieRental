c3032bef09743638991a24c6dc655b676b3f1270
Garvit Gupta <garvitgupta2001@gmail.com>
2021-07-31 21:53:42 +0530

    Update customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -5,22 +5,8 @@ const auth = require("../middleware/auth");
 const { Customer, validate } = require("../models/customer");
 const validateObjectId = require("../middleware/validateObjectId");
 
-//GET
-router.get("/", auth, async (req, res) => {
-  const customers = await Customer.find().sort("name");
-  res.send(customers);
-});
-
-router.get("/:id", [validateObjectId,auth], async (req, res) => {
-  const customer = await Customer.findById(req.params.id);
-  if (!customer) {
-    return res.status(404).send("Not found");
-  }
-  res.send(customer);
-});
-
 //POST
-router.post("/", auth, async (req, res) => {
+router.post("/", async (req, res) => {
   const error = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
